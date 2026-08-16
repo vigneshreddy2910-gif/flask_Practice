fd933beeee432aada20884ac8edabf584146189c
vigneshreddy2910-gif <vigneshreddy2910@gmail.com>
2026-08-16 12:50:24 +0530

    Added CI-CD pipeline

--- test_app.py
+++ test_app.py
@@ -61,3 +61,10 @@ def test_delete_student(client):
     response = client.get(f'/delete/{student_id}', follow_redirects=True)
     assert response.status_code == 200
     assert b"Temp User" not in response.data
+
+def test_health(client):
+    """Test health endpoint."""
+    response = client.get('/health')
+
+    assert response.status_code == 200
+    assert response.json == {"status": "healthy"}
